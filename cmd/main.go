77f6bb82a89c41cfd7541f2b316cebb72c7b1e66
shammalie <louismb@msn.com>
2023-01-01 20:22:46 +0000

    hooked up client to grpc server.

--- cmd/main.go
+++ cmd/main.go
@@ -6,12 +6,16 @@ import (
 	"strings"
 
 	"github.com/shammalie/go-network-monitor-client/internal/pcap"
+	network_capture_v1 "github.com/shammalie/go-network-monitor/pkg/network_capture.v1"
 	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 const (
-	logPrefix   = "main %v\n"
-	envFileName = "app"
+	logPrefix      = "main %v\n"
+	envFileName    = "app"
+	grpcServerAddr = "localhost:4320"
 )
 
 func main() {
@@ -27,11 +31,20 @@ func main() {
 	}
 
 	ipIgnore := viper.GetString("IP_IGNORE")
+	pcapInterface := viper.GetString("PCAP_INTERFACE")
+	pcapFilter := viper.GetString("PCAP_BPF_FILTER")
 
-	packetCapture := pcap.New("eth0", 0, true, 0, "", strings.Split(ipIgnore, ","))
+	packetCapture := pcap.New(pcapInterface, 0, true, 0, pcapFilter, strings.Split(ipIgnore, ","))
+
+	grpcClient := network_capture_v1.NewNetworkCaptureClient(grpcServerAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	for packet := range packetCapture.Capture {
-		simplePacket := pcap.Processor(packet)
-		fmt.Println(*simplePacket)
+		captureRequest := pcap.Processor(packet)
+		resp, err := grpcClient.SendNetworkCapture(captureRequest)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Println(resp)
 	}
 }
--- internal/pcap/processor.go
+++ internal/pcap/processor.go
@@ -4,49 +4,27 @@ import (
 	"fmt"
 
 	"github.com/google/gopacket"
+	network_capture_v1 "github.com/shammalie/go-network-monitor/pkg/network_capture.v1"
 )
 
-type SimplePacket struct {
-	Network     SimpleNetworkLayer
-	Transport   SimpleTransportLayer
-	Application SimpleApplicationLayer
-	Metadata    gopacket.PacketMetadata
-}
-
-type SimpleNetworkLayer struct {
-	SrcIp    string
-	DstIp    string
-	Protocol string
-}
-
-type SimpleTransportLayer struct {
-	SrcPort  string
-	DstPort  string
-	Protocol string
-}
-
-type SimpleApplicationLayer struct {
-	Protocol string
-	Payload  string
-}
-
-func Processor(packet gopacket.Packet) *SimplePacket {
+func Processor(packet gopacket.Packet) *network_capture_v1.NetworkCaptureRequest {
 	if packet.ErrorLayer() != nil {
 		fmt.Println(packet.ErrorLayer().Error())
 	}
 
-	var simpleTransportLayer SimpleTransportLayer
-	var simpleNetworkLayer SimpleNetworkLayer
-	var simpleAppliationLayer SimpleApplicationLayer
+	var simpleTransportLayer network_capture_v1.TransportLayer
+	var simpleNetworkLayer network_capture_v1.NetworkLayer
+	var simpleAppliationLayer network_capture_v1.ApplicationLayer
+	var simpleMetadata network_capture_v1.Metadata
 
 	transportLayer := packet.TransportLayer()
 	if transportLayer != nil {
-		simpleTransportLayer = processTransportLayer(packet.TransportLayer())
+		simpleTransportLayer = processTransportLayer(transportLayer)
 	}
 
 	networkLayer := packet.NetworkLayer()
 	if networkLayer != nil {
-		simpleNetworkLayer = processNetworkLayer(packet.NetworkLayer())
+		simpleNetworkLayer = processNetworkLayer(networkLayer)
 	}
 
 	applicationLayer := packet.ApplicationLayer()
@@ -54,37 +32,57 @@ func Processor(packet gopacket.Packet) *SimplePacket {
 		simpleAppliationLayer = processApplicationLayer(applicationLayer)
 	}
 
-	return &SimplePacket{
-		Network:     simpleNetworkLayer,
-		Transport:   simpleTransportLayer,
-		Application: simpleAppliationLayer,
-		Metadata:    *packet.Metadata(),
+	metadata := packet.Metadata()
+	if metadata != nil {
+		simpleMetadata = processMetadata(metadata)
+	}
+
+	return &network_capture_v1.NetworkCaptureRequest{
+		NetworkLayer:     &simpleNetworkLayer,
+		TransportLayer:   &simpleTransportLayer,
+		ApplicationLayer: &simpleAppliationLayer,
+		Metadata:         &simpleMetadata,
 	}
 }
 
-func processTransportLayer(layer gopacket.TransportLayer) SimpleTransportLayer {
+func processTransportLayer(layer gopacket.TransportLayer) network_capture_v1.TransportLayer {
 	src := layer.TransportFlow().Src().String()
 	dst := layer.TransportFlow().Dst().String()
-	return SimpleTransportLayer{
-		SrcPort:  src,
-		DstPort:  dst,
-		Protocol: layer.LayerType().String(),
+	return network_capture_v1.TransportLayer{
+		SrcPort: src,
+		DstPort: dst,
+		Protocol: &network_capture_v1.Protocol{
+			Name: layer.LayerType().String(),
+		},
 	}
 }
 
-func processNetworkLayer(layer gopacket.NetworkLayer) SimpleNetworkLayer {
+func processNetworkLayer(layer gopacket.NetworkLayer) network_capture_v1.NetworkLayer {
 	src := layer.NetworkFlow().Src().String()
 	dst := layer.NetworkFlow().Dst().String()
-	return SimpleNetworkLayer{
-		SrcIp:    src,
-		DstIp:    dst,
-		Protocol: layer.LayerType().String(),
+	return network_capture_v1.NetworkLayer{
+		SrcIp: src,
+		DstIp: dst,
+		Protocol: &network_capture_v1.Protocol{
+			Name: layer.LayerType().String(),
+		},
 	}
 }
 
-func processApplicationLayer(layer gopacket.ApplicationLayer) SimpleApplicationLayer {
-	return SimpleApplicationLayer{
-		Protocol: layer.LayerType().String(),
-		Payload:  string(layer.Payload()),
+func processApplicationLayer(layer gopacket.ApplicationLayer) network_capture_v1.ApplicationLayer {
+	return network_capture_v1.ApplicationLayer{
+		Protocol: &network_capture_v1.Protocol{
+			Name: layer.LayerType().String(),
+		},
+		Payload: layer.Payload(),
+	}
+}
+
+func processMetadata(metadata *gopacket.PacketMetadata) network_capture_v1.Metadata {
+	return network_capture_v1.Metadata{
+		Timestamp:            metadata.Timestamp.UnixMilli(),
+		CaptureLength:        int64(metadata.CaptureLength),
+		OriginalPacketLength: int64(metadata.Length),
+		Truncated:            metadata.Truncated,
 	}
 }
